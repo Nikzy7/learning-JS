1be955877ea1f603e92f81d7611876bc6780c783
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-11-29 11:31:49 +0530

    complete video 5

---
+++ 02 Basics/01Variables.js
@@ -0,0 +1,17 @@
+// string
+var fullName = "Apoorv";
+
+var courseName = "React Js Bootcamp";
+
+// boolean
+var isLoggedIn = true;
+
+// integer
+var loggedCount = 34;
+console.log(loggedCount);
+
+// asssignment
+var paymentMode;
+console.log(paymentMode);
+paymentMode = "Credit Card";
+console.log(paymentMode);
