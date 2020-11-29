2efa774feed33a43ced32f189b8e7262d3439e71
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-11-29 12:02:25 +0530

    complete video 7

---
+++ 02 Basics/03Operators.js
@@ -0,0 +1,16 @@
+var num1 = 4;
+var num2 = 5;
+
+console.log(num1 + num2);
+console.log(num1 * num2);
+
+// Use case
+var sellingPrice = 199;
+var listPrice = 799;
+var discountPercent = ((listPrice - sellingPrice) / listPrice) * 100;
+
+console.log("Discount percentage is : " + discountPercent);
+
+displayDiscountPercentage = Math.round(discountPercent);
+
+console.log(displayDiscountPercentage + "% off");
