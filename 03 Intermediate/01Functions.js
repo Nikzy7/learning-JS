ee08597091a39e0d5423f150fec51bbc1275d648
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-11-29 20:57:12 +0530

    complete video 14

---
+++ 03 Intermediate/01Functions.js
@@ -0,0 +1,22 @@
+function sayHello(){
+    console.log("Hello there Apoorv !");
+}
+
+function sayHello2(name){
+    console.log(`Hello There : ${name}`);
+}
+
+// calling
+sayHello();
+sayHello();
+// referencing
+sayHello;
+
+//using parameters
+sayHello2("negi");
+
+function namaste(){
+    return "hello in India";
+}
+
+console.log(namaste());
