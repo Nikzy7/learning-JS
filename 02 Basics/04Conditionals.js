d3cd9486f5b90891db7bdba1d29c4eacb1eaa256
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-11-29 19:58:35 +0530

    complete video 9

---
+++ 02 Basics/04Conditionals.js
@@ -0,0 +1,21 @@
+var temperature;
+
+// todo: fetch data from internet
+
+temperature  = 20;
+
+/*
+var result = temperature<20;
+console.log(result);
+*/
+
+
+if(temperature<20){
+    console.log("Its very cold outside");
+}
+
+if (temperature<30){
+    console.log("its moderate outside");
+}else{
+    console.log("its really hot outside");
+}
