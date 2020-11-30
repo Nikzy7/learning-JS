ee1b270f138811c5fdc0864d4a7ad73c9b4ba79a
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-11-30 12:51:50 +0530

    complete video 16

---
+++ 03 Intermediate/03jsContext.js
@@ -0,0 +1,21 @@
+/*
+sayHello();
+
+function sayHello(){
+    console.log("hello");
+}
+
+sayHello();
+*/
+
+if(2===2){
+    console.log("this is true");
+}
+
+var myName = "apoorv";
+
+/*
+if(myName===window.myName){
+    // will give error
+}
+*/
