e2a6de269fd58f5a4b185c45ae7ffc303647b96f
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-11-30 22:31:20 +0530

    complete video 26

---
+++ 03 Intermediate/12loops.js
@@ -0,0 +1,9 @@
+for(let i=0;i<10;i++){
+    console.log("Hello World !");
+}
+
+const myStates = ["Rajasthan","delhi","Uttarakhand","Uttar Pradesh","Assam"];
+
+for(let i=0;i<myStates.length;i++){
+    console.log(myStates[i]);
+}
