d7bbc7199c35821a5a29e160c36c01ffdcf40023
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-11-30 22:35:49 +0530

    complete video 27

--- 03 Intermediate/12loops.js
+++ 03 Intermediate/12loops.js
@@ -1,3 +1,5 @@
+/*
+// video 26
 for(let i=0;i<10;i++){
     console.log("Hello World !");
 }
@@ -7,3 +9,20 @@ const myStates = ["Rajasthan","delhi","Uttarakhand","Uttar Pradesh","Assam"];
 for(let i=0;i<myStates.length;i++){
     console.log(myStates[i]);
 }
+*/
+
+// video 27
+const myStates = ["Rajasthan","delhi","Uttarakhand","Uttar Pradesh","Assam"];
+
+let i=0;
+
+while(i<myStates.length){
+    console.log(myStates[i]);
+    i++;
+}
+
+let j=0;
+
+do{
+    console.log("Hey There !");
+}while(j!=0);
