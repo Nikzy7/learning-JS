885c92e0ce4f3e009b817ca8213ca0f93a4f2145
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-12-03 22:00:52 +0530

    complete video 43

---
+++ 05 Not Advance/03functions.js
@@ -0,0 +1,11 @@
+// self executing anonymous function
+// nobody calls it
+// gets executed once declared
+// also called IIFE
+// (Immediately Invoked Function Expression)
+
+(function (){
+    console.log("i say hello");
+    console.log("i again say hello");
+})();
+
