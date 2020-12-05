00d435b951d80889b33f499053ab209512d823c6
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-12-05 12:03:34 +0530

    complete video 50

---
+++ 06 Advancish/02strings.js
@@ -0,0 +1,8 @@
+var name = `apoorv`;
+var fullName = `${name} negi`;
+
+console.log(fullName);
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith('i'));
+console.log(fullName.includes('negi'));
+console.log(fullName.toUpperCase());
