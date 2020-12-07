a869bfc1ff9e3c670ea1e74a2cfaef1b5d64c5b4
Apoorv Negi <43514386+Nikzy7@users.noreply.github.com>
2020-12-07 12:35:55 +0530

    complete video 56

--- 06 Advancish/06classjs.js
+++ 06 Advancish/06classjs.js
@@ -18,12 +18,42 @@ class User{
     getCourseList(){
         return this.#courseList;
     }
+
+    login(){
+        return "you are logged in";
+    }
+
+    // use this to prevent child from accessing a function from parent
+    // static keyword
+    // static login(){
+    //     return "you are logged in";
+    // }
+}
+
+// inheritance
+class subAdmin extends User{
+    constructor(name,email){
+        super(name,email);
+    }
+
+    getAdminInfo(){
+        return "I am subadmin";
+    }
+
+    login(){
+        return "login for admin only";
+    }
 }
 
 module.exports = User;
 
-const rock = new User("rock","rock@rock.com");
-console.log(rock.getInfo());
-rock.enrollCourse("angular bootcamp");
-console.log(rock.getCourseList())
-console.log(rock.courseList);
+// const rock = new User("rock","rock@rock.com");
+// console.log(rock.getInfo());
+// rock.enrollCourse("angular bootcamp");
+// console.log(rock.getCourseList())
+// console.log(rock.courseList);
+
+const tom = new subAdmin("tom","tom@jerry.com");
+console.log(tom.getAdminInfo());
+console.log(tom.login());
+console.log(tom.getInfo());
